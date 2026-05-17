ef3ec5fdf0406da60f414de94818507f9a236037
jabrjj6-web <jabrjj6@icloud.com>
2026-05-17 21:51:04 +0300

    Update list_insert_pro.py

--- list_insert_pro.py
+++ list_insert_pro.py
@@ -1,7 +1,13 @@
-fruits = ["apple", "banana", "orenge"]
+print("--- List Element Insertion Demo ---\n")
 
-print("Before", fruits)
+# قائمة الفواكه بالتهجئة الصحيحة
+fruits = ["apple", "banana", "orange"]
 
+# طباعة القائمة قبل إدراج عنصر جديد
+print(f"Original List: {fruits}")
+
+# إدراج الكيوي في أول القائمة (Index 0)
 fruits.insert(0, "kiwi")
 
-print("after ", fruits)
+# طباعة القائمة بعد الإدراج
+print(f"Updated List:  {fruits}")
