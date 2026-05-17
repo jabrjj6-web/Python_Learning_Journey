2a913f592fafa8b0bc874da03d170764d34b8091
jabrjj6-web <jabrjj6@icloud.com>
2026-05-17 21:56:12 +0300

    Update list_loop_pro.py

--- list_loop_pro.py
+++ list_loop_pro.py
@@ -1,4 +1,10 @@
-names = [22,37,34,50]
+print("--- Iterating Through a List ---\n")
 
-for n in names:
-    print(n)
+# تسمية القائمة باسم يعبر عن محتواها الرقمي
+numbers = [22, 37, 34, 50]
+
+# حلقة تكرار للمرور على كل رقم وطباعته بتنسيق f-string أنيق
+for number in numbers:
+    print(f"Value: {number}")
+
+print("\nAll list items have been printed!")
