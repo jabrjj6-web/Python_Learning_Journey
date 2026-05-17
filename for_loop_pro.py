2cef8bfe4c0b2a46f93c06e4af16e19bf4587d73
jabrjj6-web <jabrjj6@icloud.com>
2026-05-17 21:30:16 +0300

    Update for_loop_pro.py

--- for_loop_pro.py
+++ for_loop_pro.py
@@ -1,2 +1,6 @@
+print("--- Loop Execution ---")
+
+# التكرار 5 مرات من 0 إلى 4
 for i in range(5):
-    print("I LIVE YOU")
+    # طباعة رقم التكرار الحالي مع النص
+    print(f"Iteration {i+1}: I LOVE YOU")
