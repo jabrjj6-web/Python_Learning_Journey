e95752acd4d446abdee44422582d09a26ddf3af1
jabrjj6-web <jabrjj6@icloud.com>
2026-05-17 21:32:56 +0300

    Update for_range_pro.py

--- for_range_pro.py
+++ for_range_pro.py
@@ -1,2 +1,7 @@
-for i in range (1,11):
-    print(i)
+print("--- Numbers Range from 1 to 10 ---\n")
+
+# حلقة التكرار تبدأ من 1 وتنتهي عند 10
+for i in range(1, 11):
+    print(f"Count: {i}")
+
+print("\nRange printing completed successfully!")
