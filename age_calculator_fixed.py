ff91c9996b22f341e408e77f46e7fc23ac891047
jabrjj6-web <jabrjj6@icloud.com>
2026-05-17 19:16:35 +0300

    Update age_calculator_fixed.py

--- age_calculator_fixed.py
+++ age_calculator_fixed.py
@@ -1,8 +1,14 @@
-print("---Age in Days calculator---")
+print("--- Age Calculator Pro ---")
 
-age_years = input("enter your age in years : ")
-age_years = int(age_years)
+age_years = int(input("Enter your age in years: "))
 
+# العمليات الحسابية
+age_months = age_years * 12
 age_days = age_years * 365
+age_hours = age_days * 24
 
-print(f"you have lived for {age_days} days.")
+# طباعة النتائج بشكل منظم
+print(f"You have lived for:")
+print(f"- {age_months} Months")
+print(f"- {age_days} Days")
+print(f"- {age_hours} Hours")
