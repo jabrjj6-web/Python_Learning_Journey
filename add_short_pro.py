47c4b837baf34ed6118a0ff0a342ed15ef9423c5
jabrjj6-web <jabrjj6@icloud.com>
2026-05-18 19:23:30 +0300

    Update add_short_pro.py

--- add_short_pro.py
+++ add_short_pro.py
@@ -1,4 +1,10 @@
 def add(x, y):
     return x + y
 
-print("The sum is:", add(10, 20))
+# نطلب من المستخدم إدخال الأرقام
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# نستدعي الدالة ونطبع الناتج
+result = add(num1, num2)
+print("The sum is:", result)
